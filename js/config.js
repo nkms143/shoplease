cb6e874c08f2651c1dc0de5094f02060c6d6e0db
nkms143 <nkms143@gmail.com>
2026-02-09 19:04:32 +0530

    fix

---
+++ js/config.js
@@ -0,0 +1,8 @@
+
+// Configuration & Secrets
+// DO NOT COMMIT THIS FILE TO GIT
+
+window.CONFIG = {
+    SUPABASE_URL: 'https://ypwyrxtxkupvmhsegscs.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwd3lyeHR4a3Vwdm1oc2Vnc2NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NzE3NTcsImV4cCI6MjA4MzI0Nzc1N30.A8xX6E2FqDyWat6yGXbUntoJA19xGZxReQzpw5cIxyk'
+};
